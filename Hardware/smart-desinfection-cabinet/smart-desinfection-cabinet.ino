22723feda479e695b1928bb5e19a6d47958a0c4a
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2026-08-27 23:50:24 +0700

    Update smart-desinfection-cabinet.ino

--- Hardware/smart-desinfection-cabinet/smart-desinfection-cabinet.ino
+++ Hardware/smart-desinfection-cabinet/smart-desinfection-cabinet.ino
@@ -1,12 +1,34 @@
 
+// UV Lamp
+const uint8_t lamp[4] = {15, 2, 14, 16};
 
+// UV Sensor
+const uint8_t uv_sensor[4] = {19, 21, 22, 23};
+
+// Gate Sensor
+const uint8_t gate_sensor[3] = {17, 5, 18};
+
+
+void setPinMode();
+
+void setLamp(int, bool);
 
 void setup() {
-  // put your setup code here, to run once:
+  setPinMode();
 
+}
+
+void setPinMode() {
+  for(byte i = 0; i < 4; i++) {
+    pinMode(lamp[i], OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void setLamp(int index, bool state = false) {
+  digitalWrite(lamp[index], state);
 }
